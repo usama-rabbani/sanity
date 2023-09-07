580245406f7221f340d901be9cf79a7428aee8c0
usama-rabbani <rabbaniusama1@gmail.com>
2023-09-07 05:18:40 -0700

    Your commit message here1

--- sanity/lib/BackgroundColorInput.ts
+++ sanity/lib/BackgroundColorInput.ts
@@ -1,19 +1,3 @@
 // components/BackgroundColorInput.js
 
-import React from 'react';
 
-const BackgroundColorInput = ({ type, value, onChange }:any) => {
-  return (
-    <div>
-      <label>Background Color</label>
-      <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        style={{ backgroundColor: value }} // Apply the background color
-      />
-    </div>
-  );
-};
-
-export default BackgroundColorInput;
