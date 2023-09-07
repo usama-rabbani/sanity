f45d0b43ce46e95e7698c578662f2fba030d11ed
usama-rabbani <rabbaniusama1@gmail.com>
2023-09-07 05:31:03 -0700

    motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     </a>
    
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-     <motion.div  initial ={{x:100}} animate={{x:[0, 50, 0]}}  transition={{duration:"2"}}  whileHover={{ opacity:0.2 }}   transition={{ duration: 0.5 }}>
+     <motion.div >
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <Link href={'/allproducts'}>
           <li  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</li>
