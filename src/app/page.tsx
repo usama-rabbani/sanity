6c04bdfb3b7223505f270bf347a1b4c8a248034b
usama-rabbani <rabbaniusama1@gmail.com>
2023-09-07 05:27:12 -0700

    ef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,7 @@ export default function Home() {
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
     </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inLinkne-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outLinkne-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinknecap="round" strokeLinknejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
+   
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
      <motion.div  initial ={{x:100}} animate={{x:[0, 50, 0]}}  transition={{duration:"2"}}  whileHover={{ opacity:0.2 }}   transition={{ duration: 0.5 }}>
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
